83e415582011973b43d2fb09f3d82bef5b07c291
Mike Nezzer <mnezz1131@gmail.com>
2019-12-30 20:07:10 -0600

    work out with video

--- Assets/word.js
+++ Assets/word.js
@@ -1,68 +1,106 @@
-//create an array of bands
-var jamBands = ["allman brothers band","big gigantic","big head todd and the monsters","chris robinson brotherhood","circles around the sun",
-"dead and company","galactic","govt mule","greensky bluegrass","lettuce","the new mastersounds","pigeons playing ping pong","robert randolph and the family band",
-"spafford","umphreys mcgee"]
+//Arrays and Variables for holding data
+var wordOptions = ["buckethead","twiddle","galactic","soulive", "khruangbin","lettuce","sunsquabi","spafford"]; 
+var selectedWord = ""; 
+var lettersInWord = []; 
+var numBlanks = 0; 
+var blanksAndSuccesses = [];
+var wrongLetters = []; 
+var letterGuessed = [];  
 
-var userGuess = []  //The letters guessed by the user 
-var computerChoice = ""; 
+//Game counters
 var winCount = 0;
-var loseCount = 0;
+var lossCount = 0;
 var guessesLeft = 10;
-var underScores = [];
-var incorrectGuess = [];
-
-
-
 
 //=============================================================================================================================
 function startGame(){
-//CREATE UNDERSCORES BASED ON THE RANDOM WORD CHOSEN BY THE COMPUTER    
-    computerChoice = jamBands[Math.floor(Math.random()*jamBands.length)]; //generates random computer from jambands array
-    console.log(computerChoice); //prints the random band chosen
+    selectedWord = wordOptions[Math.floor(Math.random()*wordOptions.length)]; //generates random computer from wordOptions array
+    console.log(selectedWord);                                                //prints the random band chosen
    
-    var splitChoice = computerChoice.split(""); //splits computerChoice string in separate characters
-    console.log(splitChoice)                    //prints the result of the computerChoice split
-  
-    for(var i=0; i < splitChoice.length; i++) {
-        underScores.push("_")                   //loop through the length to push to the underScores []
+    var lettersInWord = selectedWord.split("");                               //splits selectedWord string in separate characters
+    console.log(lettersInWord)                                                //prints the result of the selectedWord split
+                                                                              
+    numBlanks = lettersInWord.length;                                         //assigns numBlanks to the lettersInWord Array
+    console.log(numBlanks);    
+                                                                            
+//Reset    
+    guessesLeft = 10;
+    wrongLetters = [];
+    blanksAndSuccesses = [];
+
+//loop through the length to push to the blanksAndSuccesses []
+   for (var i=0; i<numBlanks; i++) {                              
+        blanksAndSuccesses.push("_")                  
     }                                            
-    console.log(underScores);       
-    document.getElementById("wordChoice").innerHTML = underScores.join(" ");  
+    console.log(blanksAndSuccesses);       
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");  
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;  
+    document.getElementById("lossCounter").innerHTML = lossCount;
 }
 
-    startGame();
-//==============================================================================================================================
-var guessed = function(){
-    document.getElementById("guesses").innerHTML = guessesLeft;
-    }  
-      
-    guessed();
+function checkLetters (letter) {
+    var isLetterInWord = false; 
+    //Checks to see if letter exists at all in word
 
-    var guessedSoFar = function(){
-        document.getElementById("guessessofar").innerHTML = userGuess.join(",");
-        }  
-        guessedSoFar();
+    for (var i=0; i< numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+          isLetterInWord = true;
+        }
+    }
+//check where letter exists
+if(isLetterInWord) {
+    for (var i=0; i<numBlanks; i++) {
+        if(selectedWord[i] == letter) {
+            blanksAndSuccesses[i] = letter;
+        }
+    }
+    
+}
 
-//==============================================================================================================================   
+else{
+    wrongLetters.push(letter);
+    guessesLeft--
+}
+
+console.log(blanksAndSuccesses)
+
+
+}
+
+function roundComplete () {
+  //  console.log("Win Count: " + winCount + "| Loss Count: " + lossCount  + "| Guesses Left: "+ guessesLeft);
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
+ //blanks And Successes is consoled on line 67
+ //letterInWord is consoled on line 21
+   
+  if(lettersInWord.toString() == blanksAndSuccesses.toString()) {
+        winCount++;
+        alert("YOU WON!");
+
+         document.getElementById("winCounter").innerHTML = winCount;
+        startGame();
+    }
+
+  else if (guessesLeft == 0){
+    lossCount++;
+  alert("YOU LOST!!");
+    document.getElementById("lossCounter").innerHTML = lossCount;
+  startGame();
+   }
+}
+
+//Main Process==============================================================================================================================
+startGame();
+
 document.onkeyup = function(event) {
-    var userGuess = event.key;
-    var letterKeyed = event.key.toLowerCase();
-    console.log(letterKeyed);
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+ 
+    checkLetters(letterGuessed);
+    roundComplete();
+   
 
-    //document.getElementById("guessedsofar").innerHTML = letterKeyed.join(" ");
-guessesLeft--;
-guessed(); 
-
-if(computerChoice.indexOf(letterKeyed) > -1) {
-    console.log(true);
-    underScores[computerChoice.indexOf(letterKeyed)] = letterKeyed;
-    console.log(underScores);
-    underScores[0].innerHTML = underScores.join(' ');
-    document.getElementById("wordChoice").innerHTML = underScores.join(" ");
-} else {incorrectGuess.push(letterKeyed);
-console.log(incorrectGuess)
-guessedSoFar();
-document.getElementById("guessessofar").innerHTML = incorrectGuess.join(",");
-}
 
 }
